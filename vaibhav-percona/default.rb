b1137c3c5a947fb192ec36721bbae14fdebeab1b
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-08 19:00:32 +0530

    changes

--- vaibhav-percona/default.rb
+++ vaibhav-percona/default.rb
@@ -26,7 +26,23 @@ execute 'grant-user' do
 end
 
 
+execute 'xtrabackup' do
+  command 'sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A'
+  command 'sudo sh -c "echo 'deb http://repo.percona.com/apt trusty main' > /etc/apt/sources.list.d/percona.list"'
+  command 'sudo sh -c "echo 'deb-src http://repo.percona.com/apt trusty main' >> /etc/apt/sources.list.d/percona.list"'
+  command 'sudo apt-get update'
+  command 'sudo apt-get install percona-xtrabackup'
+end
+
+
 #here rhel and debian specific home directory can be specified
 execute 'base-backup' do
   command 'innobackupex --no-timestamp --user=newuser  --password=v /home/ubuntu/base-backup'
 end
+
+
+execute 'incremental' do
+  command 'innobackupex --incremental /home/ubuntu/incremental --incremental-basedir=/home/ubuntu/base-backup --user=newuser --password=v'
+end
+
+
