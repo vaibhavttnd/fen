6c7bed07c73ec43bf79589712b8b108fdde4ec8e
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-08 19:46:43 +0530

    notifies

--- vaibhav-percona/recipes/default.rb
+++ vaibhav-percona/recipes/default.rb
@@ -1,28 +1,36 @@
 
-
-
 execute 'percona_install' do
   command 'wget https://repo.percona.com/apt/percona-release_0.1-3.$(lsb_release -sc)_all.deb'
   command 'dpkg -i percona-release_0.1-3.$(lsb_release -sc)_all.deb'
+  notifies :run, 'execute[apt_update]', :immediately
 end
 
 execute 'apt-update' do
   command 'sudo apt-get update'
+  notifies :run, 'execute[install_percona]', :immediately
+  action :nothing
 end
 
 
-execute 'apt-update' do
+execute 'install_percona' do
   command 'apt-get install percona-server-server-5.5'
+  notifies :run, 'execute[create-user]', :immediately
+  action :nothing
 end
 
 
 execute 'create-user' do
   command "/usr/bin/mysql -u root -pv -e'CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'v';'"
+  notifies :run, 'execute[grant-user]', :immediately
+  action :nothing
 end
 
 
 execute 'grant-user' do
   command "/usr/bin/mysql -u root -pv -e'GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';FLUSH PRIVILEGES;'"
+  notifies :run, 'execute[xtrabackup]', :immediately
+  action :nothing
+
 end
 
 
@@ -32,17 +40,21 @@ execute 'xtrabackup' do
   command "echo 'deb-src http://repo.percona.com/apt trusty main' >> /etc/apt/sources.list.d/percona.list"
   command 'sudo apt-get update'
   command 'sudo apt-get install percona-xtrabackup'
+  notifies :run, 'execute[base-backup]', :immediately
+  action :nothing
+
 end
 
 
 #here rhel and debian specific home directory can be specified
 execute 'base-backup' do
   command 'innobackupex --no-timestamp --user=newuser  --password=v /home/ubuntu/base-backup'
+  notifies :run, 'execute[incremental]', :immediately
+  action :nothing
 end
 
 
 execute 'incremental' do
   command 'innobackupex --incremental /home/ubuntu/incremental --incremental-basedir=/home/ubuntu/base-backup --user=newuser --password=v'
+  action :nothing
 end
-
-
