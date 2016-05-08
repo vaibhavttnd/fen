88b84061d0365d293c4820e3f0961b176f2c6176
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-08 21:25:08 +0530

    minor

--- vaibhav-percona/recipes/default.rb
+++ vaibhav-percona/recipes/default.rb
@@ -30,14 +30,14 @@ end
 
 
 execute 'create-user' do
-  command "sudo /usr/bin/mysql -u root -pv -e\"CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'v';\""
+  command "/usr/bin/mysql -u root -pv -e\"CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'v';\""
   notifies :run, 'execute[grant-user]', :immediately
   action :nothing
 end
 
 
 execute 'grant-user' do
-  command "sudo /usr/bin/mysql -u root -pv -e'GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';FLUSH PRIVILEGES;'"
+  command "/usr/bin/mysql -u root -pv -e'GRANT ALL PRIVILEGES ON * . * TO 'newuser'@'localhost';FLUSH PRIVILEGES;'"
   notifies :run, 'execute[xtrabackup]', :immediately
   action :nothing
 
