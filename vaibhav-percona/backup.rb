6c0b23c346080b326b00977ccd91d0ce8f148dcb
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-09 10:03:18 +0530

    changes

--- vaibhav-percona/backup.rb
+++ vaibhav-percona/backup.rb
@@ -23,6 +23,9 @@ execute 'install_percona' do
   command "echo 'percona-server-server-5.5 percona-server-server/root_password password v' | sudo debconf-set-selections"
   command "echo 'percona-server-server-5.5 percona-server-server/root_password_again password v' | sudo debconf-set-selections"
   command 'sudo apt-get install percona-server-server-5.5 -y'  
+  command 'sudo apt-get -f install'
+#  notifies :run, 'execute[user]', :immediately
+#  action :nothing
 end
 
 #user='/home/ubuntu/user.sql'
@@ -33,6 +36,8 @@ file '/home/ubuntu/user.sql' do
   owner 'root'
   group 'root'
   notifies :run, 'execute[create-user]', :immediately
+#  notifies :run, 'execute[permission]', :immediately
+#  action :nothing
 end
 
 #permission='/home/ubuntu/permission.sql'
@@ -42,20 +47,8 @@ file '/home/ubuntu/permission.sql' do
   owner 'root'
   group 'root'
   notifies :run, 'execute[grant-user]', :immediately
-end
-
-file '/home/ubuntu/newdb.sql' do
-  content 'CREATE DATABASE incremental;'
-  mode '0755'
-  owner 'root'
-  group 'root'
-  notifies :run, 'execute[db-create]', :immediately
-end
-
-execute 'db-create' do
-  command "mysql -u root -pv < /home/ubuntu/newdb.sql"
-  notifies :run, 'execute[incremental]', :immediately
-  action :nothing
+#  notifies :run, 'execute[create-user]', :immediately
+#  action :nothing
 end
 
 
@@ -64,6 +57,7 @@ end
 
 execute 'create-user' do
   command "mysql -u root -pv < /home/ubuntu/user.sql"
+#  notifies :run, 'execute[grant-user]', :immediately
   action :nothing
 end
 
@@ -74,6 +68,16 @@ execute 'grant-user' do
   action :nothing
 end
 
+
+#execute 'xtrabackup' do
+#  command 'sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A'
+#  command "echo 'deb http://repo.percona.com/apt trusty main' > /etc/apt/sources.list.d/percona.list"
+#  command "echo 'deb-src http://repo.percona.com/apt trusty main' >> /etc/apt/sources.list.d/percona.list"
+#  command 'sudo apt-get update'
+#  command 'sudo apt-get install percona-xtrabackup -y'
+#  notifies :run, 'execute[base-backup]', :immediately
+#  action :nothing
+#end
 
 
 execute 'xtrabackup' do
@@ -112,7 +116,7 @@ end
 #here rhel and debian specific home directory can be specified
 execute 'base-backup' do
   command 'sudo innobackupex --no-timestamp --user=opswork  --password=v /home/ubuntu/base-backup'
-#  notifies :run, 'execute[incremental]', :immediately
+  notifies :run, 'execute[incremental]', :immediately
   action :nothing
 end
 
