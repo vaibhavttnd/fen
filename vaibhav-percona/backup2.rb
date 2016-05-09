82b96e8f13d437719f959528da218e645dcde65f
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-09 10:27:12 +0530

    changes

--- vaibhav-percona/backup2.rb
+++ vaibhav-percona/backup2.rb
@@ -23,32 +23,39 @@ execute 'install_percona' do
   command "echo 'percona-server-server-5.5 percona-server-server/root_password password v' | sudo debconf-set-selections"
   command "echo 'percona-server-server-5.5 percona-server-server/root_password_again password v' | sudo debconf-set-selections"
   command 'sudo apt-get install percona-server-server-5.5 -y'  
-  command 'sudo apt-get -f install'
-#  notifies :run, 'execute[user]', :immediately
-#  action :nothing
 end
 
 #user='/home/ubuntu/user.sql'
 
 file '/home/ubuntu/user.sql' do
-  content 'CREATE USER \'opswork\'@\'localhost\' IDENTIFIED BY \'v\';'
+  content 'CREATE USER \'vaibhav\'@\'localhost\' IDENTIFIED BY \'v\';'
   mode '0755'
   owner 'root'
   group 'root'
   notifies :run, 'execute[create-user]', :immediately
-#  notifies :run, 'execute[permission]', :immediately
-#  action :nothing
 end
 
 #permission='/home/ubuntu/permission.sql'
 file '/home/ubuntu/permission.sql' do
-  content 'GRANT ALL PRIVILEGES ON * . * TO \'opswork\'@\'localhost\';FLUSH PRIVILEGES;'
+  content 'GRANT ALL PRIVILEGES ON * . * TO \'vaibhav\'@\'localhost\';FLUSH PRIVILEGES;'
   mode '0755'
   owner 'root'
   group 'root'
   notifies :run, 'execute[grant-user]', :immediately
-#  notifies :run, 'execute[create-user]', :immediately
-#  action :nothing
+end
+
+file '/home/ubuntu/newdb.sql' do
+  content 'CREATE DATABASE incremental;'
+  mode '0755'
+  owner 'root'
+  group 'root'
+  notifies :run, 'execute[db-create]', :immediately
+end
+
+execute 'db-create' do
+  command "mysql -u root -pv < /home/ubuntu/newdb.sql"
+  notifies :run, 'execute[incremental]', :immediately
+  action :nothing
 end
 
 
@@ -57,7 +64,6 @@ end
 
 execute 'create-user' do
   command "mysql -u root -pv < /home/ubuntu/user.sql"
-#  notifies :run, 'execute[grant-user]', :immediately
   action :nothing
 end
 
@@ -68,16 +74,6 @@ execute 'grant-user' do
   action :nothing
 end
 
-
-#execute 'xtrabackup' do
-#  command 'sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A'
-#  command "echo 'deb http://repo.percona.com/apt trusty main' > /etc/apt/sources.list.d/percona.list"
-#  command "echo 'deb-src http://repo.percona.com/apt trusty main' >> /etc/apt/sources.list.d/percona.list"
-#  command 'sudo apt-get update'
-#  command 'sudo apt-get install percona-xtrabackup -y'
-#  notifies :run, 'execute[base-backup]', :immediately
-#  action :nothing
-#end
 
 
 execute 'xtrabackup' do
@@ -115,13 +111,13 @@ end
 
 #here rhel and debian specific home directory can be specified
 execute 'base-backup' do
-  command 'sudo innobackupex --no-timestamp --user=opswork  --password=v /home/ubuntu/base-backup'
-  notifies :run, 'execute[incremental]', :immediately
+  command 'sudo innobackupex --no-timestamp --user=vaibhav  --password=v /home/ubuntu/base-backup'
+  notifies :run, 'execute[db-create]', :immediately
   action :nothing
 end
 
 
 execute 'incremental' do
-  command 'sudo innobackupex --incremental /home/ubuntu/incremental --incremental-basedir=/home/ubuntu/base-backup --user=opswork --password=v'
+  command 'sudo innobackupex --incremental /home/ubuntu/incremental --incremental-basedir=/home/ubuntu/base-backup --user=vaibhav --password=v'
   action :nothing
 end
