24dabd4f3aeb89a3618c59c0353559e544e738a4
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-13 15:36:17 +0530

    fen

--- cookbook-fen/recipes/user.rb
+++ cookbook-fen/recipes/user.rb
@@ -9,29 +9,29 @@ end
 
 #################### User created
 
-node['web_app']['user_name'] = "monitoring"
-node['web_app']['group_name'] = "monitoring"
-node['web_app']['user_dir'] = "/home/monitoring"
+#node['web_app']['user_name'] = "monitoring"
+#node['web_app']['group_name'] = "monitoring"
+#node['web_app']['user_dir'] = "/home/monitoring"
 
-directory "#{node['web_app']['user_dir']}/.ssh" do
-  mode 0775
-  user node['web_app']['user_name']
-  group node['web_app']['group_name']
-#  action :create
-  subscribes :action, 'user[monitoring]', :immediately    #check this 
-end
+#directory "#{node['web_app']['user_dir']}/.ssh" do
+#  mode 0775
+#  user node['web_app']['user_name']
+#  group node['web_app']['group_name']
+##  action :create
+#  subscribes :action, 'user[monitoring]', :immediately    #check this 
+#end
 
 ####################  Directory created
 
-cookbook_file "#{node['web_app']['user_dir']}/.ssh/id_rsa" do
-  source 'private.txt'      ######## delete private.txt
+#cookbook_file "#{node['web_app']['user_dir']}/.ssh/id_rsa" do
+#  source 'private.txt'      ######## delete private.txt
 #  cookbook 'fen-apache2'   ######## change it after testing
-  mode 0600
-  user node['web_app']['user_name']
-  group node['web_app']['group_name']
-#  action :create
-  subscribes :action, "directory[#{node['web_app']['user_dir']}/.ssh]", :immediately    #check this
-end
+#  mode 0600
+#  user node['web_app']['user_name']
+#  group node['web_app']['group_name']
+##  action :create
+#  subscribes :action, "directory[#{node['web_app']['user_dir']}/.ssh]", :immediately    #check this
+#end
 
 
 ####################  Private Key passed
