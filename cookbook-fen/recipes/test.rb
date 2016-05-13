87cf7abb04ab4c2b03997a011a9230d9e90e13c8
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-13 23:37:12 +0530

    changes

--- cookbook-fen/recipes/test.rb
+++ cookbook-fen/recipes/test.rb
@@ -9,3 +9,19 @@ cookbook_file "/home/monitoring/ami_backup.sh" do
 end
 
 
+
+include_recipe 'cron'
+env = { AWS_DEFAULT_REGION: 'us-east-1' }
+exepath = '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
+cron 'ami_and_snapshot_ami_backup' do
+  comment 'ami and snapshot backup every 15 days'
+  environment env
+  path exepath
+  day '*/15'
+  user 'monitoring'
+  command 'bash /home/monitoring/ami_backups.sh'
+#  mailto 'system-alerts@fen.com'
+#  subscribes :action, "cookbook_file[#{node['web_app']['user_dir']}/ami_backup.sh]", :immediately
+end
+
