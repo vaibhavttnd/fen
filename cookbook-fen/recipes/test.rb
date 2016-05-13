7b8c8d9f9ddc83b4047e8731ee668ec529dcc36b
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-13 23:24:14 +0530

    changes

--- cookbook-fen/recipes/test.rb
+++ cookbook-fen/recipes/test.rb
@@ -1,34 +1,11 @@
-node['web_app']['user_name'] = "monitoring"
-node['web_app']['group_name'] = "monitoring"
-node['web_app']['user_dir'] = "/home/monitoring"
 
 
-
-cookbook_file "#{node['web_app']['user_dir']}/ami_backup.sh" do
+cookbook_file "/home/monitoring/ami_backup.sh" do
   source 'ami_backup.sh'
   mode 0700
-  user node['web_app']['user_name']
-  group node['web_app']['group_name']
+  user 'monitoring'
+  group 'monitoring' 
   action :create
-end
-
-cookbook_file "#{node['web_app']['user_dir']}/ami_list.txt" do
-  source 'ami_list.txt'
-  mode 0700
-  user node['web_app']['user_name']
-  group node['web_app']['group_name']
-  action :create
-#  subscribes :action, "cookbook_file[#{node['web_app']['user_dir']}/ami_backup.sh]", :immediately
-end
-
-
-cookbook_file "#{node['web_app']['user_dir']}/ami_delete.sh" do
-  source 'ami_delete.sh'
-  mode 0700
-  user node['web_app']['user_name']
-  group node['web_app']['group_name']
-  action :create
-#  subscribes :action, "cookbook_file[#{node['web_app']['user_dir']}/ami_list.txt]", :immediately
 end
 
 
