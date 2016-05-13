3e8a73400a8d287bf70e4cff9c5ccfcb5e1ba835
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-05-13 15:44:19 +0530

    fen

--- cookbook-fen/recipes/user.rb
+++ cookbook-fen/recipes/user.rb
@@ -9,17 +9,17 @@ end
 
 #################### User created
 
-#node['web_app']['user_name'] = "monitoring"
-#node['web_app']['group_name'] = "monitoring"
-#node['web_app']['user_dir'] = "/home/monitoring"
+node['web_app']['user_name'] = "monitoring"
+node['web_app']['group_name'] = "monitoring"
+node['web_app']['user_dir'] = "/home/monitoring"
 
-#directory "#{node['web_app']['user_dir']}/.ssh" do
-#  mode 0775
-#  user node['web_app']['user_name']
-#  group node['web_app']['group_name']
-##  action :create
-#  subscribes :action, 'user[monitoring]', :immediately    #check this 
-#end
+directory "#{node['web_app']['user_dir']}/.ssh" do
+  mode 0775
+  user node['web_app']['user_name']
+  group node['web_app']['group_name']
+#  action :create
+  subscribes :action, 'user[monitoring]', :immediately    #check this 
+end
 
 ####################  Directory created
 
